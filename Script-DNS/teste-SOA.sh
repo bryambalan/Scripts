90cd94aa6fa7807d5300c430d813d892ad994780
Bryam <bryambalan@outlook.com.br>
2019-09-14 15:54:17 -0300

    Update teste-SOA.sh

--- Script-DNS/teste-SOA.sh
+++ Script-DNS/teste-SOA.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-#arquivo=`cat /home/made4it/script/zonas | head -$1 | tail -1`
-FinalIndex=`wc -l /home/made4it/script/zonas | cut -d' ' -f1`
+FinalIndex=`wc -l /home/made4it/script/zonas-aut | cut -d' ' -f1`
 
 for j in $(seq 1 $FinalIndex);
 do
-	linhas=`cat /home/made4it/script/zonas | head -$j | tail -1`
-#	echo $linhas
-	 echo -e "\n !!!!!!!!!!!!!! Teste da Zona  $linhas$i !!!!!!!!!!!!!!!!!!\n"
-	 for i in {0..255};
- 	 do
-  		 dig @localhost -x $linhas$i +short
-       	done
+	linhas=`cat /home/made4it/script/zonas-aut | head -$j | tail -1`
+	 echo -e "\n !!!!!!!!!!!!!! Teste da Zona  $linhas !!!!!!!!!!!!!!!!!!\n"
+  		 dig @localhost SOA $linhas +short
 done
